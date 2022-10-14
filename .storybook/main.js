698172b9d276e620eb59a2fec6da06cb55ef4c3a
uiuing <uiuing@foxmail.com>
2022-10-14 16:24:23 +0800

    init story template
    Signed-off-by: uiuing <uiuing@foxmail.com>

---
+++ .storybook/main.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "stories": [
+    "../packages/semi-ui/**/_story/*.stories.@(js|jsx|ts|tsx|svelte)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/svelte"
+}
---
+++ packages/semi-ui/button/Button.svelte
@@ -0,0 +1,27 @@
+<script>
+  import './button.css';
+  import { createEventDispatcher } from 'svelte';
+
+  export let primary = false;
+  export let backgroundColor;
+  export let size = 'medium';
+  export let label = '';
+
+  let mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+  let style = backgroundColor ? `background-color: ${backgroundColor}` : '';
+
+  const dispatch = createEventDispatcher();
+
+  function onClick(event) {
+    dispatch('click', event);
+  }
+</script>
+
+<button
+  type="button"
+  class={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
+  {style}
+  on:click={onClick}>
+  {label}
+</button>
